e60a530fc06a7b2cbce292207004dd9d8daec8cb
Anurag Yadav <anuragyadav622003@gmail.com>
2025-11-12 17:50:05 +0530

    vercel

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -1,7 +1,7 @@
 // frontend/src/lib/api.ts
 import axios from 'axios';
 
-const API_BASE_URL =  'http://localhost:3000'
+const API_BASE_URL =  'https://dataforge-platform.vercel.app'
 
 export const api = axios.create({
   baseURL: API_BASE_URL,
