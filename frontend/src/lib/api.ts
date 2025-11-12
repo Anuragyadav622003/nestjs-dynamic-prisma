5b80c1c7b955b686b9172abe3624478897c02d72
Anurag Yadav <anuragyadav622003@gmail.com>
2025-11-12 15:16:15 +0530

    add backend

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -1,7 +1,7 @@
 // frontend/src/lib/api.ts
 import axios from 'axios';
 
-const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3000/api';
+const API_BASE_URL = "https://dataforge-platform.vercel.app"  //||process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3001/api'
 
 export const api = axios.create({
   baseURL: API_BASE_URL,
