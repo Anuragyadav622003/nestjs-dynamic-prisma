2d4e4902a8f5c6c4e8957d0b533b3b08ca0c9306
Anurag Yadav <anuragyadav622003@gmail.com>
2025-11-12 16:19:03 +0530

    vercel

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -13,7 +13,7 @@ async function bootstrap() {
   }));
   
   app.enableCors({
-    origin: ['http://localhost:3001', 'https://dataforge-platform-c2tj-m8ipwet5i-anuragyadav622003s-projects.vercel.app'],
+    origin: ["https://dataforge-platform-c2tj.vercel.app",'http://localhost:3001'],
     credentials: true,
   });
  
