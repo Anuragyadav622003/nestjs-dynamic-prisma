ceda2cd6473274b5e89753730650ac51784497da
Anurag Yadav <anuragyadav622003@gmail.com>
2025-11-13 17:16:22 +0530

    backend

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,9 +3,11 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { DynamicModelsModule } from './dynamic-models/dynamic-models.module';
 import { AppController } from './app.controller';
+import { AppService } from './app.service';
 
 @Module({
   imports: [AuthModule, DynamicModelsModule],
-  controllers:[AppController]
+  controllers:[AppController],
+  providers:[AppService]
 })
 export class AppModule {}
